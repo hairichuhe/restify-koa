3f641012fb697676ce1b9ba0a4ff8415ed9f9ea6
hairichuhe <14320794@qq.com>
2016-11-23 17:40:46 +0800

    1123司接

--- koa-demo2.js
+++ koa-demo2.js
@@ -4,5 +4,13 @@ const router=require("koa-router");
 
 //主页输出hello world
 router.get("/",function(ctx,next){
+	console.log("主页get请求")
 	ctx.body="hello world"
+});
+
+app.use(router.routes(),router.allowMethods())
+
+var server=app.listen(8082,function(){
+	var host=server.address().address;
+	var port
 })
