5ae1b47457133cab5d4048e36d59e9986934df0a
hairichuhe <14320794@qq.com>
2016-11-25 17:30:43 +0800

    1125公司接

--- koa-demo2.js
+++ koa-demo2.js
@@ -2,11 +2,14 @@ const koa=require("koa");
 const app=new koa();
 const router=require("koa-router")();
 const fs=require("fs");
+const bodyParser=require("koa-bodyparser");
 const readFile = file =>{
 	return new Promise((resolve,reject)=>{
 		fs.readFile(file,(err,data)=>err?reject(err):resolve(data))
 	})
-}
+};
+
+app.use(bodyParser());
 
 //主页输出hello world
 router.get("/",function(ctx,next){
@@ -50,19 +53,54 @@ router.get("/ab*cd",(ctx,next)=>{
 // 	ctx.body=fs.createReadStream("get.html").toString();
 // })
 
-router.get("/get", async (ctx,next)=>{
-	let content = await readFile("get.html");
-	ctx.body=content;
+// router.get("/get", async (ctx,next)=>{
+// 	let content = await readFile("get.html");
+// 	ctx.body=content;
+// })
+
+// router.get("/get.html", async (ctx,next)=>{
+// 	let content = await readFile("get.html");
+// 	ctx.body=content;
+// })
+
+router.get("/get.html", (ctx,next)=>{
+	let content = fs.readFileSync("get.html");
+	ctx.body=content.toString();
 })
 
 // process_get
 router.get("/process_get",function(ctx,next){
 	var response={
-		frist_name:ctx.query.frist_name,
+		first_name:ctx.query.first_name,
 		last_name:ctx.query.last_name
-	}
-	ctx.body=JSON.stringify(response);
+	};
+	ctx.body=response;
 })
+
+router.get("/post.html",(ctx,next)=>{
+	let content= fs.readFileSync("post.html").toString();
+	ctx.body=content;
+})
+
+router.post("/process_post",(ctx,next)=>{
+	console.log(ctx.request.body)
+	let res={
+		first_name:ctx.request.body.first_name,
+		last_name:ctx.request.body.last_name
+	};
+	ctx.body=res;
+})
+
+router.get("/file.html",(ctx,next)=>{
+	let res=fs.readFileSync("file.html").toString();
+	ctx.body=res;
+})
+
+router.post("/file_upload",(ctx,next)=>{
+	console.log(ctx.request);
+	ctx.body={"name":"yaya"}
+})
+
 app.use(router.routes(),router.allowedMethods())
 
 var server=app.listen(8082,function(){
