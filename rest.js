4f6ed4b5be1632da4820bc52b35abbd7a027819a
王晓军 <14320794@qq.com>
2016-12-05 07:04:26 +0800

    周末更改

--- rest.js
+++ rest.js
@@ -43,6 +43,36 @@ router.get("/addUser",async (ctx,next)=>{
 	ctx.body=user;
 })
 
+router.get("/delUser",async (ctx,next)=>{
+	let res={};
+	let read=function(){
+		return new Promise((resolve,reject)=>{
+			fs.readFile(__dirname+"/users.json","utf8",(err,data)=>{
+				data=JSON.parse(data);
+				delete data["user2"];
+				res=data;
+				resolve(true)
+			})
+		})
+	};
+
+	await read();
+	ctx.body=res;
+})
+
+router.get("/:id",async (ctx,next)=>{
+	let read=function(){
+		return new Promise((resolve,reject)=>{
+			fs.readFile(__dirname+"/users.json","utf8",function(err,data){
+				data=JSON.parse(data);
+				resolve(data["user"+ctx.params.id])
+			})
+		})
+	}
+
+	ctx.body=await read();
+})
+
 app.use(router.routes(),router.allowedMethods());
 
 const server = app.listen(8082,function(){
