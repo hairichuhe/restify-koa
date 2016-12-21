e3114358e69c11fdafe2360f42d618ccd371ad6e
hairichuhe <14320794@qq.com>
2016-12-21 17:33:35 +0800

    1221

---
+++ getIp.js
@@ -0,0 +1,17 @@
+// var os = require('os');  
+// var IPv4,hostName;  
+// hostName=os.hostname();  
+// console.log(os.networkInterfaces());
+// // for(var i in os.networkInterfaces()){  
+// //     if(os.networkInterfaces().eth0[i].family=='IPv4'){  
+// //         IPv4=os.networkInterfaces().eth0[i].address;  
+// //     }  
+// // }  
+// console.log('----------local IP: '+IPv4);  
+// console.log('----------local host: '+hostName);
+
+
+// require('getmac').getMac(function(err,macAddress){
+//     if (err)  throw err
+//     console.log(macAddress)
+// })
